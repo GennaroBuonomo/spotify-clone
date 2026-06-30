08e5b70f8d48d15469222b8c89278c55932e24a5
GennaroBuonomo <gennarobuonomo66@gmail.com>
2026-06-30 16:23:41 +0200

    add songsData.js with initial song entries

---
+++ src/songsData.js
@@ -0,0 +1,29 @@
+export const songsData = [
+  {
+    id: 1,
+    title: "Sad But True",
+    artist: "Metallica",
+    album: "Metallica (Black Album)",
+    cover: "https://images.unsplash.com/photo-1614613535308-eb5fbd3d2c17?q=80&w=300&auto=format&fit=crop",
+    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    duration: "6:12"
+  },
+  {
+    id: 2,
+    title: "Enter Sandman",
+    artist: "Metallica",
+    album: "Metallica (Black Album)",
+    cover: "https://images.unsplash.com/photo-1514525253161-7a46d19cd819?q=80&w=300&auto=format&fit=crop",
+    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+    duration: "7:05"
+  },
+  {
+    id: 3,
+    title: "Master of Puppets",
+    artist: "Metallica",
+    album: "Master of Puppets",
+    cover: "https://images.unsplash.com/photo-1598387993441-a364f854c3e1?q=80&w=300&auto=format&fit=crop",
+    audioUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+    duration: "8:35"
+  }
+];
