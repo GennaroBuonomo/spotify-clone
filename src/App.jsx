292d59a3ddee23bc2e8f244d14d0dab887f4a1fd
GennaroBuonomo <gennarobuonomo66@gmail.com>
2026-06-30 16:28:07 +0200

    implement initial layout with sidebar and main view in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { songsData } from './songsData';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className="spotify-layout">
+
+      {/* SideBar (sinistra)*/}
+      <aside className="sidebar">
+        <h2>Libreria</h2>
+        <ul>
+          <li>Playlist 1</li>
+          <li>Playlist 2</li>
+        </ul>
+      </aside>
+
+      {/* 2. MAIN VIEW (Centro/Destra) */}
+      <main className="main-view">
+        <header className="main-header">
+          <h1>Buongiorno</h1>
+        </header>
+      </main>
+    </div>
   )
 }
 
